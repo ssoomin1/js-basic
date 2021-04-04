218bc1e5f9b503fc8bf6fe155d218a0c68575ae7
Soomin Shin <ssoomin1@naver.com>
2021-04-04 23:24:41 +0900

    check quiz answer and study

--- array-api-ver2.js
+++ array-api-ver2.js
@@ -1,1 +1,106 @@
 //quiz정답
+//이해하고 손에 익힐 것. 기술면접과 실무에 굉장히 많이 적용되는 예제들임.
+// Q1. make a string out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const result=fruits.join(',');
+    console.log(result);
+    }
+    
+    // Q2. make an array out of a string
+    {
+    const fruits = '🍎, 🥝, 🍌, 🍒';
+    const result=fruits.split(',',2); //limit은 필수적이진 않다. 
+    console.log(result);
+    }
+    
+    // Q3. make this array look like this: [5, 4, 3, 2, 1]
+    {
+    const array = [1, 2, 3, 4, 5];
+    const reverse_array=array.reverse();
+    console.log(reverse_array);
+    console.log(array); //배열자체를 리버스 시키는구나.
+    }
+    
+    // Q4. make new array without the first two elements
+    {
+     const array = [1, 2, 3, 4, 5];
+    // const result=array.splice(0,2); //splice는 return값이 삭제된 것이 리턴된다. 배열자체는 삭제된다. 
+    // console.log(result);
+    // console.log(array);
+     const result=array.slice(2,5); //원하는 부분만
+     console.log(result);
+    }
+    
+    class Student {
+    constructor(name, age, enrolled, score) {
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
+    }
+    const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 66),
+    new Student('E', 18, true, 88),
+    ];
+    
+    // Q5. find a student with the score 90
+    {
+        const result=students.find((student)=>student.score === 90); //true가 되면 그 배열의 요소를 넘겨준다.
+        console.log(result);
+    }
+    
+    // Q6. make an array of enrolled students
+    {
+        const result=students.filter((student) => student.enrolled);
+        console.log(result);
+    }
+    
+    // Q7. make an array containing only the students' scores
+    // result should be: [45, 80, 90, 66, 88]
+    {
+        const result=students.map((student)=>student.score);
+        console.log(result);
+    }
+    
+    // Q8. check if there is a student with the score lower than 50 
+    {   
+        //50점보다 낮은 사람이 있는지 없는지 확인해서 bool값을 리턴하여 하나라도 조건에 만족되면 true
+        const result=students.some((student)=>student.score <50);
+        console.log(result);
+
+        //모든게 만족되어야한다면 every사용하기
+        const result2=!students.every((student)=>student.score>=50);
+        console.log(result2);
+    }
+    
+     // Q9. compute students' average score
+    { 
+        const result=students.reduce((prev,curr)=>prev+curr.score,0);
+        console.log(result / students.length);
+        //reduceRight : 순서가 거꾸로
+    }
+    
+    // Q10. make a string containing all the scores
+    // result should be: '45, 80, 90, 66, 88'
+    {
+        const result=students.map((student)=>student.score)
+        //.filter((score)=>score > 50 )
+        .join(',');
+        console.log(result);
+    }
+    
+    // Bonus! do Q10 sorted in ascending order
+    // result should be: '45, 66, 80, 88, 90'
+    {
+        const result=students.map((student)=>student.score)
+        .sort((a,b)=>a-b)
+        .join(',');
+        console.log(result);
+        //sort안의 값이 마이너스이면 오름차순 정렬이고 0이면 같은거, positive이면 내림차순 정렬을 해줄것.
+    }
+    

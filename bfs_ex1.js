1ab3f78ac47716c9e185313a2b8fa6256be1a256
Soomin Shin <ssoomin1@naver.com>
2021-04-06 06:00:47 +0900

    bfs_study

--- bfs_ex1.js
+++ bfs_ex1.js
@@ -1,4 +1,23 @@
 //BFS를 알고리즘을 만들어보기
-const graph={
-    A:['B']
+'use strict'
+
+function bfs(graph,start,visited){
+
 }
+
+
+const graph=[
+    []
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited=[false]*9
+
+bfs(graph,1,visited)

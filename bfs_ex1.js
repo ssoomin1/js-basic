f037708822df0fd6f3b02b50710c210c4c5c2571
Soomin Shin <ssoomin1@naver.com>
2021-04-06 10:03:19 +0900

    dfs_ex1.js 작성

--- bfs_ex1.js
+++ bfs_ex1.js
@@ -1,23 +1,32 @@
-//BFS를 알고리즘을 만들어보기
-'use strict'
+//BFS를 알고리즘을 만들어보기 => 큐,FIFO
+'use strict';
 
-function bfs(graph,start,visited){
+const graph={
+    0: [],
+    1:[2,3,8],
+    2:[1,7],
+    3:[1,4,5],
+    4:[3,5],
+    5:[3,4],
+    6:[7],
+    7:[2,6,8],
+    8:[1,7]
+};
 
-}
+const bfs=(graph,startNode)=>{
+    let needVisit=[]; //탐색해야하는 노드 [1]
+    let visited=[]; //이미 탐색한 노드 []
+    
+    needVisit.push(startNode);
+    while(needVisit.length !==0 ){ //탐색해야할 노드가 남아있다면 
+        const node=needVisit.shift(); //앞에서부터 빼내기
+        if(!visited.includes(node)){
+            visited.push(node);
+            needVisit=[...needVisit,...graph[node]];
+        }
+    }
+    return visited;  
+};
 
-
-const graph=[
-    []
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-    [7],
-    [2,6,8],
-    [1,7]
-]
-
-visited=[false]*9
-
-bfs(graph,1,visited)
+console.log('BFS알고리즘 -FIFO')
+console.log(bfs(graph,1));
---
+++ dfs_ex1.js
@@ -0,0 +1,27 @@
+//DFS를 알고리즘으로 표현하기  => 스택,LIFO
+'use strict';
+
+const graph=[
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+
+function dfs(graph,start){
+    let needVisited=[];
+    let visited=[];
+
+}
+
+
+dfs(graph,1);
+
+
+

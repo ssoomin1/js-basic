a4b95287db3070b01bc0e9702283bb2d534ff869
Soomin Shin <ssoomin1@naver.com>
2021-04-07 07:42:56 +0900

    js100제 6번까지

--- dfs_ex1.js
+++ dfs_ex1.js
@@ -1,25 +1,33 @@
-//DFS를 알고리즘으로 표현하기  => 스택,LIFO
+//DFS를 알고리즘으로 표현하기  => 스택,LIFO 에러가 나는 이유는?
 'use strict';
 
-const graph=[
-    [],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-    [7],
-    [2,6,8],
-    [1,7]
-]
+const graph={
+    0:[],
+    1:[2,3,8],
+    2:[1,7],
+    3:[1,4,5],
+    4:[3,5],
+    5:[3,4],
+    6:[7],
+    8:[1,7]
+};
 
+const dfs=(graph,startNode)=>{
+    let needVisitStack=[]; //탐색을 해야할 노드들
+    let visitedQueue=[]; //탐색을 마친 노드들
 
-function dfs(graph,start){
-    let needVisited=[];
-    let visited=[];
+    needVisitStack.push(startNode);
 
-}
-
+    //탐색을 해야할 노드들이 남아있다면
+    while(needVisitStack.length !== 0 ){
+        const node=needVisitStack.pop(); //bfs는 여기서 shift를 사용했었음
+        if(!visitedQueue.includes(node)){
+            visitedQueue.push(node);
+            needVisitStack=[...needVisitStack, ...graph[node]];
+        }
+    }
+    return visitedQueue;
+};
 
 dfs(graph,1);
 
---
+++ js_100/ex1.js
@@ -0,0 +1,37 @@
+{
+console.log('#1');
+const nums=[100,200,300,400,500];
+nums.pop();
+nums.pop();
+console.log(nums);
+}
+
+{
+console.log('#2');
+const arr1=[200,100,300];
+arr1.splice(2,0,10000);
+console.log(arr1);
+}
+
+{
+console.log('#3');
+const arr2=[100,200,300];
+console.log(typeof(arr2)); //number가 아니라 object 타입
+}
+
+{
+    //이런 함정이!!
+    console.log('#5');
+    const a=10;
+    const b=2;
+
+    for(let i=1;i<5;i+=2){
+        a+=i;  //i=1,i=3 11+3=14
+    }
+
+    console.log(a+b); //14+2=16
+}
+
+
+
+

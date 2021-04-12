937e47824434c0d35e381bdaec53bb09a573fb68
Soomin Shin <ssoomin1@naver.com>
2021-04-13 07:43:10 +0900

    문제 33번 풀이

--- js_100/ex31-ex50.js
+++ js_100/ex31-ex50.js
@@ -1,7 +1,13 @@
 //ex31 => 시간복잡도 문제
 //ex32 : 단어수 세기
 {
-    const sentence = prompt('입력: ').split(' ');
+    //const sentence = prompt('입력: ').split(' ');
+    const sentence = '안녕하세요. 저는 미림여정에 다니는 신수민입니다.'.split(' ');
     console.log(sentence.length);
 }
 
+//ex33 : 거꾸로 출력하기
+{
+    const numbers=prompt('입력: ').split(' ');
+    console.log(numbers.reverse().join(' '));
+}
